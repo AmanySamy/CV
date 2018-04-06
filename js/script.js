bb27d718fe27614d3dfe0feac838f2537369b0a7
Amany samy <amanysamy628@yahoo.com>
2018-04-06 10:21:13 +0200

    first commit

--- js/script.js
+++ js/script.js
@@ -4,18 +4,21 @@ $(function() {
 	'use strict';
 	
 	$('html').niceScroll();
+	//$(".main-page").niceScroll();
 
 	$('[data-toggle="tooltip"]').tooltip();
 
 	$(".port").hover(
 		function(){
-		$(this).children().first().slideDown();
+		$(this).children().first().show(500);
 		},
 		function(){
-		$(this).children().first().slideUp();
+		$(this).children().first().hide(500);
 		}
 	);
 
+	
 	$(".main-page").css("height",$(".side-page").height());
+	$(".main-page").niceScroll();
 
 });
